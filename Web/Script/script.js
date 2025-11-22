9524f745ddcab87642719aa95bbd045b7e3c149f
Muhammad Abu-Bakr <abubakrmuhammed075@gmail.com>
2025-11-22 02:52:06 -0800

    Refactor BMI Calculator form: Enhance structure, improve button functionality, and update styles for better usability

--- Web/Script/script.js
+++ Web/Script/script.js
@@ -1,11 +1,25 @@
+const btn1=document.getElementById("btnStandard");
+const btn2=document.getElementById("btnMetric");
+const calculateWeight=document.getElementById("weightLogic");
+const calculateHeight=document.getElementById("heightLogic");
 function cickBtn(btn){
-    const btn1=document.getElementById("btnStandard");
-    const btn2=document.getElementById("btnMetric");
     btn.style.color = 'red';
-        if (btn === btn1) {
+    logic(btn);
+    if (btn === btn1) {
         btn2.style.color = 'black';
         
     } else {
         btn1.style.color = 'black';
     }
 }
+function logic(btn){
+    if (btn === btn1) {
+        calculateWeight.innerHTML='<label for=""><input type="number" placeholder="pounds" required>pounds</label>'
+        calculateHeight.innerHTML='<label for=""><input type="number" max="9" min="1" placeholder="feet" required>feet</label><label for=""><input type="number" max="9" min="1" placeholder="inch" required>inches</label>'
+    } else {
+        calculateWeight.innerHTML='<label for=""><input type="number" placeholder="kg" required>Kilogram</label>'
+        calculateHeight.innerHTML='<label for=""><input type="number" placeholder="cm" required>cm</label>'
+    }
+
+}
+
